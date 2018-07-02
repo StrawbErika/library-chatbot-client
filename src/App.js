e53ed2fdf03449f9a55ddc8ce89a15eab633a709
Erika Nepomuceno <eanepomuceno@up.edu.ph>
2018-07-02 15:30:23 +0800

    - deleted usuals, + textbox works

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Input } from 'antd';
 
 class App extends Component {
   render() {
+    const { TextArea } = Input;
+
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <TextArea rows={4} />
+        </div>
       </div>
     );
   }
