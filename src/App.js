bfc3c252a09a7f2a7fb17bb033b821df3f0e2d47
Erika Nepomuceno <eanepomuceno@up.edu.ph>
2018-07-03 16:03:59 +0800

    + send button, modal, +borrow & all checkbox

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Input } from 'antd';
+import { Input, Button, Icon } from 'antd';
+import './App.css';
+import SendModal from './SendModal';
 
 class App extends Component {
   render() {
@@ -7,13 +9,17 @@ class App extends Component {
 
 
     return (
-      <div className="App">
-        <div>
-          <TextArea rows={4} />
+      <div id = "container">
+        <div id = "logo">
+        </div>
+        <div id = "input-button">
+          <TextArea rows = {10} id = "text-area"/>
+          <SendModal/>
         </div>
       </div>
     );
   }
 }
+
 
 export default App;
