236a18b58c4dffd19e394fca70e41793e41dd0e2
Erika Nepomuceno <eanepomuceno@up.edu.ph>
2018-07-03 21:52:15 +0800

    + basic UI done without assets

--- src/SendModal.js
+++ src/SendModal.js
@@ -4,63 +4,54 @@ import './App.css';
 import UserCheckbox from './UserCheckbox';
 
 class SendModal extends React.Component {
-  state = {
-    ModalText: 'Content of the modal',
-    visible: false,
-    confirmLoading: false,
-  }
-
-  showModal = () => {
-    this.setState({
-      visible: true,
-    });
-  }
-
-  handleOk = () => {
-    this.setState({
-      ModalText: 'The modal will be closed after two seconds',
-      confirmLoading: true,
-    });
-    setTimeout(() => {
-      this.setState({
+    state = {
+        loading: false,
         visible: false,
-        confirmLoading: false,
-      });
-    }, 2000);
-  }
-
-  handleCancel = () => {
-    console.log('Clicked cancel button');
-    this.setState({
-      visible: false,
-    });
-  }
-
-  onChange = (checkedValues) => {
-    console.log('checked = ', checkedValues);
-  }
+      }
     
-  render() {
-    const { visible, confirmLoading, ModalText } = this.state;
-    return (
-      <div>
-        <Button type="primary" onClick={this.showModal} id="send"> Send </Button>
-        <Modal title="Send to Users"
-          visible={visible}
-          onOk={this.handleOk}
-          confirmLoading={confirmLoading}
-          onCancel={this.handleCancel}
-          width={850}
-          id="send-modal"
-        >
-            <div id="modal-content">
-                <UserCheckbox/> 
-            </div>
-
-        </Modal>
-      </div>
-    );
-  }
-}
-
+      showModal = () => {
+        this.setState({
+          visible: true,
+        });
+      }
+    
+      handleOk = () => {
+        this.setState({ loading: true });
+        setTimeout(() => {
+          this.setState({ loading: false, visible: false });
+        }, 3000);
+      }
+    
+      handleCancel = () => {
+        this.setState({ visible: false });
+      }
+    
+      render() {
+        const { visible, loading } = this.state;
+        return (
+          <div>
+            <Button type="primary" onClick={this.showModal} id="send"> Send </Button>
+              
+            <Modal
+              visible={visible}
+              title="Send to Users"
+              onOk={this.handleOk}
+              onCancel={this.handleCancel}
+              width={850}
+              footer={[
+                <Button key="back" onClick={this.handleCancel}>Return</Button>,
+                <Button key="submit" type="primary" loading={loading} onClick={this.handleOk}>
+                  Submit
+                </Button>,
+              ]}
+            >
+            
+            <UserCheckbox/>
+            </Modal>
+          </div>
+        );
+      }
+    }
 export default SendModal;
+
+
